67c58f136f3e5a66798d8d238e5c14970a00dc20
Olav Markus Sjursø <ollii_007@hotmail.com>
2018-09-13 17:30:26 +1000

    Created occ and c table functions for search and decoding

--- assignment_2/bwtsearch/bwtsearch.cpp
+++ assignment_2/bwtsearch/bwtsearch.cpp
@@ -26,6 +26,69 @@ void print_usage(char* argv[])
               << "\t" << " 'delimiter' encoded_file index_folder -i 'record_id' (diplay contents of record)" << std::endl;
 }
 
+/*
+ * constructs a c table (implemented as a map in this case) from
+ * the encoded bwt string. The C[c] contains the number of characters
+ * that are alphabetically smaller than c.
+ *
+ * @param s encoded bwt string
+ */
+std::map<char, unsigned int> construct_c_table(const std::string &s)
+{
+    std::map<char, unsigned int> c_table;
+
+    for (char c : s)
+        if(c_table.find(c) == c_table.end())
+            for (char ch : s)
+                if (ch < c)
+                    c_table[c]++;
+
+    return c_table;
+}
+
+
+/*
+ * returns the rank (number of occurrences) of a character c in the range [1, n]
+ * in the encoded bwt string, s.
+ *
+ * @param c search character
+ * @param n from index 1 to n
+ * @param s encoded bwt string
+ */
+int rank(const char c, const unsigned int n, const std::string &s)
+{
+    int occ = 0;
+    int i = 0;
+
+    for (char ch : s)
+    {
+        if (i >= n)
+            break;
+
+        if(ch == c)
+            occ++;
+
+        i++;
+    }
+
+    return occ;
+}
+
+
+void num_matching_substrings(const char &d, const std::string &input_file, const std::string &index_folder, const std::string &query) {}
+void num_matching_records(const char &d, const std::string &input_file, const std::string &index_folder, const std::string &query) {}
+void list_identifiers(const char &d, const std::string &input_file, const std::string &index_folder, const std::string &query) {}
+void display_records(const char &d, const std::string &input_file, const std::string &index_folder, const int[] ids) {}
+
+
+/*
+ * Args:
+ * 1. delimiter
+ * 2. input_file
+ * 3. index_folder
+ * 4. option
+ * 5. query search_term
+ */
 
 int main(int argc, char* argv[])
 {
@@ -34,6 +97,14 @@ int main(int argc, char* argv[])
         print_usage(argv);
         exit(1);
     }
+
+    std::string str = argv[1];
+    char d = '0';
+
+    if (str.find("\\n") != std::string::npos)
+        d = '\n';
+    else
+        d = str.at(3);
 
     try
     {
